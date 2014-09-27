bea625b39582083d353710f4f1a8fb322e93f1dc
Sarah Vessels <sarah.vessels@cirrusmio.com>
2014-09-27 14:17:14 -0400

    Include fonts in dist/

--- Gruntfile.js
+++ Gruntfile.js
@@ -389,7 +389,7 @@ module.exports = function (grunt) {
             '*.html',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
-            'fonts/*'
+            'styles/fonts/*'
           ]
         }, {
           expand: true,
