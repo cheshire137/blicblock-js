8827ae57564ff8c8a2fa7fc7c7a8f8cc9530cc30
Sarah Vessels <sarah.vessels@cirrusmio.com>
2014-10-11 13:42:30 -0400

    Add Location#with_country
    Also strip trailing and preceding whitespace from country.

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,10 +3,19 @@ class Location < ActiveRecord::Base
 
   before_create :capitalize_country
 
+  def self.with_country country
+    locations = arel_table
+    lowercase_country = Arel::Nodes::NamedFunction.new('LOWER',
+                                                       [locations[:country]])
+    location = where(lowercase_country.eq(country.downcase.strip).to_sql).first
+    return location if location
+    create(country: country)
+  end
+
   private
 
   def capitalize_country
     return unless country
-    self.country = country.split.map(&:capitalize).join(' ')
+    self.country = country.split.map(&:capitalize).join(' ').strip
   end
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -12,8 +12,46 @@ RSpec.describe Location, type: :model do
   end
 
   it 'adjusts capitalization of country on save' do
-    new_location = build(:location, country: 'THESE caps ARE wEiRD')
+    new_location = build(:location, country: "\n\tTHESE caps ARE wEiRD   ")
     new_location.save!
     expect(new_location.reload.country).to eq('These Caps Are Weird')
+  end
+
+  describe 'with_country' do
+    let(:country) { "UnItEd STATES\r\n" }
+
+    context 'when matching location does not already exist' do
+      it 'returns a saved record' do
+        expect(Location.with_country(country)).to be_persisted
+      end
+
+      it 'creates a new record' do
+        expect { Location.with_country(country) }.
+            to change(Location, :count).by(1)
+      end
+
+      it 'returns a record with adjusted capitalization' do
+        expect(Location.with_country(country).country).
+            to eq('United States')
+      end
+    end
+
+    context 'when matching location already exists' do
+      let!(:location) { create(:location, country: country) }
+
+      it 'returns a saved record' do
+        expect(Location.with_country(country)).to be_persisted
+      end
+
+      it 'does not create a new record' do
+        expect { Location.with_country(country) }.
+            to_not change(Location, :count)
+      end
+
+      it 'returns a record with adjusted capitalization' do
+        expect(Location.with_country(country).country).
+            to eq('United States')
+      end
+    end
   end
 end
