f8d092e39d3a803a841c772f6567654986d568c8
Sarah Vessels <cheshire137@gmail.com>
2016-10-30 15:26:33 -0500

    Flesh out remaining left L tetromino tests

--- spec/javascripts/models/tetromino_checker_spec.es6.js
+++ spec/javascripts/models/tetromino_checker_spec.es6.js
@@ -62,10 +62,45 @@ describe('TetrominoChecker', () => {
   //  *  1    *   **1
   // **  ***  *     *
   // A   B    C   D
-  it('returns true when left L A tetromino exists')
-  it('returns true when left L B tetromino exists')
-  it('returns true when left L C tetromino exists')
-  it('returns true when left L D tetromino exists')
+  it('returns true when left L A tetromino exists', () => {
+    const b1 = new Block({ x: 0, y: 1, color: 'blue' })
+    const b2 = new Block({ x: 1, y: 1, color: 'blue' })
+    const b3 = new Block({ x: 2, y: 0, color: 'blue' })
+    const b4 = new Block({ x: 2, y: 1, color: 'blue' })
+    const blocks = [b1, b2, b3, b4]
+    const checker = new TetrominoChecker(blocks, 0)
+    expect(checker.check()).toEqual(true)
+  })
+
+  it('returns true when left L B tetromino exists', () => {
+    const b1 = new Block({ x: 0, y: 0, color: 'blue' })
+    const b2 = new Block({ x: 1, y: 0, color: 'blue' })
+    const b3 = new Block({ x: 1, y: 1, color: 'blue' })
+    const b4 = new Block({ x: 1, y: 2, color: 'blue' })
+    const blocks = [b1, b2, b3, b4]
+    const checker = new TetrominoChecker(blocks, 0)
+    expect(checker.check()).toEqual(true)
+  })
+
+  it('returns true when left L C tetromino exists', () => {
+    const b1 = new Block({ x: 0, y: 0, color: 'blue' })
+    const b2 = new Block({ x: 0, y: 1, color: 'blue' })
+    const b3 = new Block({ x: 1, y: 0, color: 'blue' })
+    const b4 = new Block({ x: 2, y: 0, color: 'blue' })
+    const blocks = [b1, b2, b3, b4]
+    const checker = new TetrominoChecker(blocks, 0)
+    expect(checker.check()).toEqual(true)
+  })
+
+  it('returns true when left L D tetromino exists', () => {
+    const b1 = new Block({ x: 0, y: 2, color: 'blue' })
+    const b2 = new Block({ x: 1, y: 2, color: 'blue' })
+    const b3 = new Block({ x: 0, y: 1, color: 'blue' })
+    const b4 = new Block({ x: 0, y: 0, color: 'blue' })
+    const blocks = [b1, b2, b3, b4]
+    const checker = new TetrominoChecker(blocks, 0)
+    expect(checker.check()).toEqual(true)
+  })
 
   // 1        *1
   // *     1   *  1**
