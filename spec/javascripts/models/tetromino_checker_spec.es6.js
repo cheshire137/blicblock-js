ad3fb8b66a2d78a0b33e8a97ad2992dff225143e
Sarah Vessels <cheshire137@gmail.com>
2016-10-30 15:20:03 -0500

    Add pending tests for remaining tetrominos

--- spec/javascripts/models/tetromino_checker_spec.es6.js
+++ spec/javascripts/models/tetromino_checker_spec.es6.js
@@ -9,6 +9,7 @@ describe('TetrominoChecker', () => {
     expect(checker.check()).toEqual(false)
   })
 
+  // 1***
   it('returns true when straight horizontal tetromino exists', () => {
     const b1 = new Block({ x: 0, y: 0, color: 'blue' })
     const b2 = new Block({ x: 0, y: 1, color: 'blue' })
@@ -19,6 +20,10 @@ describe('TetrominoChecker', () => {
     expect(checker.check()).toEqual(true)
   })
 
+  // 1
+  // *
+  // *
+  // *
   it('returns true when straight vertical tetromino exists', () => {
     const b1 = new Block({ x: 0, y: 0, color: 'blue' })
     const b2 = new Block({ x: 1, y: 0, color: 'blue' })
@@ -29,6 +34,8 @@ describe('TetrominoChecker', () => {
     expect(checker.check()).toEqual(true)
   })
 
+  // 1*
+  // **
   it('returns true when square tetromino exists', () => {
     const b1 = new Block({ x: 0, y: 0, color: 'blue' })
     const b2 = new Block({ x: 0, y: 1, color: 'blue' })
@@ -38,4 +45,40 @@ describe('TetrominoChecker', () => {
     const checker = new TetrominoChecker(blocks, 0)
     expect(checker.check()).toEqual(true)
   })
+
+  //  1       1*
+  //  *  1    *   **1
+  // **  ***  *     *
+  // A   B    C   D
+  it('returns true when left L A tetromino exists')
+  it('returns true when left L B tetromino exists')
+  it('returns true when left L C tetromino exists')
+  it('returns true when left L D tetromino exists')
+
+  // 1        *1
+  // *     1   *  1**
+  // **  ***   *  *
+  // A   B     C  D
+  it('returns true when right L A tetromino exists')
+  it('returns true when right L B tetromino exists')
+  it('returns true when right L C tetromino exists')
+  it('returns true when right L D tetromino exists')
+
+  //           1    1
+  // *1    1*  **  **
+  //  **  **    *  *
+  //  A   B    C   D
+  it('returns true when Z A tetromino exists')
+  it('returns true when Z B tetromino exists')
+  it('returns true when Z C tetromino exists')
+  it('returns true when Z D tetromino exists')
+
+  // 1    1
+  // **  **   1   *1*
+  // *    *  ***   *
+  // A    B   C    D
+  it('returns true when T A tetromino exists')
+  it('returns true when T B tetromino exists')
+  it('returns true when T C tetromino exists')
+  it('returns true when T D tetromino exists')
 })
