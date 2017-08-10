6d63372d15881e07c0512c110e30857ceb6308ca
Sarah Vessels <cheshire137@gmail.com>
2017-08-10 00:16:55 -0500

    Drop user, pass for test, dev dbs

--- db/schema.rb
+++ db/schema.rb
@@ -17,16 +17,16 @@ ActiveRecord::Schema.define(version: 20141011173528) do
   enable_extension "plpgsql"
 
   create_table "locations", force: :cascade do |t|
-    t.string   "country"
+    t.string   "country",      limit: 255
     t.string   "country_code", limit: 3
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "scores", force: :cascade do |t|
-    t.string   "initials"
-    t.integer  "value",       null: false
-    t.string   "ip_address"
+    t.string   "initials",    limit: 255
+    t.integer  "value",                   null: false
+    t.string   "ip_address",  limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "location_id"
